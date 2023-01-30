78b9ced4cc8b37a5e16aaf2918b7253a75d58d0b
EstefanyMel0 <estefanymelo028@gmail.com>
2023-01-30 14:29:23 -0400

    novo codigo

---
+++ python_basico/exc01.py
@@ -0,0 +1,18 @@
+'''
+EXERCICIO 01: Faça um formulário que solicite o nome, cpf, endereço, idade, altura e telefone e imprima isso em um relatório organizado.
+'''
+
+nome = input("Informe seu nome:\n")
+cpf = input("Informe seu cpf:\n")
+endereço = input("Informe seu endereço:\n")
+idade = input("Informe sua idade:\n")
+altura = input("Informe sua altura:\n")
+telefone = input("Informe seu telefone:\n")
+
+print("------------RELATÓRIO GERAL------------")
+print("Nome:\t\t", nome)
+print("Idade:\t\t", idade)
+print("CPF:\t\t", cpf)
+print("Altura:\t\t", altura)
+print("Telefone:\t", telefone)
+print("Endereço:\t", endereço)
